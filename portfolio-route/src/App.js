30aa9353c8773dda186c7dc4c46b30f7e752d07b
Vildan Beyza Çetin <vcetin1356@gmail.com>
2023-12-19 22:14:13 +0300

    last

--- portfolio-route/src/App.js
+++ portfolio-route/src/App.js
@@ -1,26 +1,32 @@
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
 import Projects from "./pages/projects/Projects";
 import NotFound from "./components/error/NotFound";
+import Layout from "./components/Layout";
 function App() {
   return (
     <BrowserRouter>
-     <div class="home-bgImg-container">
 
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contacts" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-     </div>
+        <Routes>
+          <Route
+            path=""
+            element={
+              <Layout/>
+            }
+          >
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contacts" element={<Contact />} />
+          </Route>     
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
     </BrowserRouter>
   );
 }
