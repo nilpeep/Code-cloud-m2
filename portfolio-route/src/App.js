757ca000dd3c9862bc0526ac194d70ae88675c70
Vildan Beyza Çetin <vcetin1356@gmail.com>
2023-12-19 20:47:35 +0300

    second commit

--- portfolio-route/src/App.js
+++ portfolio-route/src/App.js
@@ -1,13 +1,25 @@
-import Footer from "./components/footer/Footer"
-import Navbar from "./components/navbar/Navbar"
-
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Projects from "./pages/projects/Projects";
+import NotFound from "./components/error/NotFound";
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <Footer/>
-    </div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contacts" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- portfolio-route/src/components/footer/Footer.jsx
+++ portfolio-route/src/components/footer/Footer.jsx
@@ -2,7 +2,24 @@ import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="horizontal">
+  <div className="social-icons">
+    <a href="#">
+      <i className="fab fa-twitter fa-2x" />
+    </a>
+    <a href="#">
+      <i className="fab fa-facebook fa-2x" />
+    </a>
+    <a href="#">
+      <i className="fab fa-instagram fa-2x" />
+    </a>
+    <a href="#">
+      <i className="fab fa-github fa-2x" />
+    </a>
+  </div>
+  <div className="copyright">© Copyright 2023</div>
+</footer>
+
   )
 }
 
