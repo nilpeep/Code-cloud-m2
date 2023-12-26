2ad566d7521d1a54e77268b1c113e32a2f362a3a
nilpeep <121940876+nilpeep@users.noreply.github.com>
2023-12-26 20:48:01 +0300

    genres

--- moviecloud/src/components/MovieCard/MovieCard.jsx
+++ moviecloud/src/components/MovieCard/MovieCard.jsx
@@ -3,17 +3,54 @@ import { Container, Card, Col, Row } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 
 function MovieCard() {
+  const [movies, setMovies] = useState([]);
+
+  const navigate = useNavigate();
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const baseUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
+
+  const getMovies = () => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((res) => setMovies(res.results));
+  };
+
+  useEffect(() => {
+    getMovies(baseUrl);
+  }, []);
+
   return (
-    <div class="search-box">
-      <form>
-        <input
-          type="text"
-          class="search-input"
-          placeholder="Search for movies..."
-          style={{ color: "black" }}
-        />
-        <i class="search-icon fas fa-search"></i>
-      </form>
+    <div>
+      <div className="search-box">
+        <form>
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search for movies..."
+            style={{ color: "black" }}
+          />
+          <i className="search-icon fas fa-search"></i>
+        </form>
+      </div>
+      <Row>
+        {movies.map((movie) => (
+          <Col key={movie.id} style={{ padding: "0.5rem" }}>
+            <Card
+              style={{ width: "12rem" }}
+              onClick={() => navigate(`/movies/${movie.id}`)}
+            >
+              <Card.Img
+                src={
+                  movie.poster_path
+                    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                    : "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+                }
+              />
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
--- moviecloud/src/pages/MovieDetail/MovieDetail.jsx
+++ moviecloud/src/pages/MovieDetail/MovieDetail.jsx
@@ -7,80 +7,39 @@ import "./MovieDetail.css";
 
 const MovieDetail = () => {
   const { id } = useParams();
-
-  const navigate = useNavigate();
+  console.log(id);
 
   const [movie, setMovie] = useState([]);
-
-  const [error, setError] = useState(false);
-
-  const [loading, setLoading] = useState(true);
 
   const apiKey = process.env.REACT_APP_API_KEY;
 
   const getMovie = () => {
     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
-      .then((response) => {
-        if (!response.ok) {
-          setError(true);
-          throw new Error("wrong");
-        }
-        return response.json();
-      })
-      .then((response) => {
-        setLoading(false);
-        setMovie(response);
-        console.log(response);
-      })
-      .catch((err) => console.error(err));
+      .then((response) => response.json())
+      .then((res) => setMovie(res));
   };
 
   useEffect(() => {
     getMovie();
   }, []);
 
-  if (error) {
-    return <NotFound />;
-  } else if (loading) {
-    return <Spinner />;
-  } else {
-    return (
-      <div className="box">
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt=""
-        />
-        <div style={{ margin: "1rem" }}>
-          <div>
-            <span className="title" style={{ fontWeight: "bolder" }}>
-              {movie.original_title}
-            </span>
-            <span className="date">({movie.release_date.substring(0, 4)})</span>
-          </div>
-          <div>
-            <span>
-              {movie.genres.map((genre, index) => {
-                if (index === 0) {
-                  return <span>{genre.name}</span>;
-                } else if (index === movie.genres.length - 1) {
-                  return <span> {genre.name} •</span>;
-                } else {
-                  return <span> ,{genre.name}</span>;
-                }
-              })}
-            </span>
-          </div>
-          <p style={{ color: "rgb(200,200,255)" }}>{movie.overview}</p>
-          <button onClick={() => navigate(-1)} className="btn btn-warning">
-            back
-          </button>
-          <button onClick={() => navigate("/")} className="btn btn-primary">
-            other movies
-          </button>
-        </div>
+  console.log(movie.genres);
+
+  return (
+    <div className="box">
+      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
+
+      <div>
+        <span className="title">{movie.original_title}</span>
+        <span>({movie.release_date})</span>
       </div>
-    );
-  }
+      {/* <div>
+        {movie.genres.map((genre) => (
+          <span>{genre.name}</span>
+        ))}
+      </div> */}
+    </div>
+  );
 };
 
 export default MovieDetail;
